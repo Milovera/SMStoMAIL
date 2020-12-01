47b19101df533d11d1055ba48e72c8e02700cbc7
melovera <pavelidj@gmail.com>
2020-12-01 17:38:52 +0300

    done with UI

---
+++ app/src/main/java/com/example/smstomail/PreferencesKeys.kt
@@ -0,0 +1,9 @@
+package com.example.smstomail
+
+object PreferencesKeys {
+    const val LOGIN_KEY = "LOGIN"
+    const val PASSW_KEY = "PASSW"
+    const val SERVER_KEY = "SERVER"
+    const val SSL_PORT = "SSL_PORT"
+    const val RECIPIENTS = "RECIPIENTS"
+}
